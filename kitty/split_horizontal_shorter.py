bccab78e24c02de45f3d0a58812f9b95f63b9839
Renier Morales <renier@morales-rodriguez.net>
2024-05-26 02:15:51 -0500

    kitty splits with resize

---
+++ kitty/split_horizontal_shorter.py
@@ -0,0 +1,30 @@
+import os
+from kittens.tui.handler import result_handler
+from kitty.fast_data_types import current_focused_os_window_id
+
+
+def main():
+    pass
+
+
+@result_handler(no_ui=True)
+def handle_result(args, result, target_window_id, boss):
+    w1 = boss.window_id_map.get(target_window_id)
+    if w1 is None:
+        return
+
+    boss.call_remote_control(w1, ("launch", "--cwd=current", "--location=hsplit"))
+
+    # get currently focused window
+    w2 = boss.active_tab.active_window
+    if w2 is None:
+        return
+
+    boss.call_remote_control(
+        w2,
+        ("resize-window", "--self", "--axis=reset"),
+    )
+    boss.call_remote_control(
+        w2,
+        ("resize-window", "--self", "--increment=-10", "--axis=vertical"),
+    )
